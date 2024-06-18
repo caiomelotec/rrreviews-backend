254f14060a44064177e9d58563b4a4a3c0b917a4
caiomelotec <caio.melotec@icloud.com>
2024-06-18 14:48:45 +0200

    timestamp

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,19 +1,12 @@
 import mongoose, { Schema, model } from "mongoose";
 
-// interface IUser {
-//   _doc?: {
-//     _id: mongoose.ObjectId;
-//     username: string;
-//     email: string;
-//     password: string;
-//   };
-// }
-
-const userSchema = new Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  sessionToken: { type: String },
-});
+const userSchema = new Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
 export default model("User", userSchema);
